0b1d96bda19e06c773e63e1e5046774add73d278
Mario de la Ossa <mariodelaossa@gmail.com>
2016-02-06 19:59:16 -0600

    Initial entry point for parser

---
+++ bank_scraper.rb
@@ -0,0 +1,32 @@
+require 'optparse'
+require 'json'
+
+# Defaults
+options = {
+    login: {}
+}
+
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby bank_scraper.rb [options] bank'
+
+  opts.on '-l', '--login_data', '=DATA', 'Your bank\'s required login data in JSON format' do |l|
+    options[:login] = JSON.parse l
+  end
+
+  opts.separator ""
+
+  opts.on_tail '-h', '--help', 'Show this message' do
+    puts opts
+    exit
+  end
+end.parse!
+
+bank = ARGV[0]
+require_relative "./banks/#{bank}.rb"
+
+# Get our bank class object
+bank = Object.const_get(bank.split('_').collect(&:capitalize).join).new(options)
+
+bank.accounts.each do |account|
+  puts account.number
+end
