0220ef4142c96abb5e5f7dd58d157cfc5aec6760
Mario de la Ossa <mariodelaossa@gmail.com>
2016-02-07 02:22:06 -0600

    [BacNicaragua] Add `accounts` method

--- banks/bac_nicaragua.rb
+++ banks/bac_nicaragua.rb
@@ -4,6 +4,12 @@ require_relative '../lib/bank'
 
 class BacNicaragua < Bank
 
+  ACCOUNT_TYPES = {
+      banking: /^CUENTAS BANCARIAS/,
+      credit: /^CUENTAS DE CRÉDITO/,
+      loans: /^PRÉSTAMOS/
+  }
+
   def initialize(data)
     super
 
@@ -23,6 +29,40 @@ class BacNicaragua < Bank
     raise SignInError, "Wrong Username or Password" if @scraper.text_field(name: 'pass').exists?
     raise SignInError, "Throttled, too many sessions" if @scraper.url == "https://www1.sucursalelectronica.com/ebac/common/showSessionRestriction.go"
     logger.debug "signed in"
+  end
+
+  def accounts
+    @accounts ||=
+      begin
+
+        accounts = {}
+        types = @scraper.tables(id: 'resultsTableOnTop')
+        types.each do |type|
+          account_type = ACCOUNT_TYPES.select {|k,v| v =~ type.trs.first.text}.keys.first # We just want the key...
+          accounts[account_type] = []
+          logger.debug "Found account type: #{account_type}"
+
+          type.trs.each do |account|
+            # If not empty, it's a header. Skip
+            next unless ACCOUNT_TYPES.select {|k,v| v=~ account.text}.empty?
+            next if account.spans.first.class_name == 'tableTitle' # Skip title rows
+
+            data = account.spans
+
+            logger.debug "Adding new account: #{data.map &:text}"
+
+            acc = Account.new
+            acc.currency = data[4].text == 'COR' ? 'NIO' : data[4].text
+            acc.number = data[2].text.to_i
+            acc.name = data[1].text
+            acc.balance = data[3].text.to_f
+            acc.id = account.forms[1].input(name: 'productId').value
+
+            accounts[account_type] << acc
+          end
+        end
+        accounts
+      end
   end
 
 end
